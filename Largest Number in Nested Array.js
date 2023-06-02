cd4d62a8976f9122b71d92bf60d14472f73ec155
georgesroberto <georgesroberto21@gmail.com>
2023-06-02 15:50:54 +0300

    Several Function Approaches Used

---
+++ Largest Number in Nested Array.js
@@ -0,0 +1,38 @@
+/*
+*Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i]. */
+function largestOfFour(arr) {
+    const results = [];
+    for (let i = 0; i < arr.length; i++) {
+      let largestNumber = arr[i][0];
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > largestNumber) {
+          largestNumber = arr[i][j];
+        }
+      }
+      results[i] = largestNumber;
+    }
+  
+    return results;
+}
+
+function largestOfFour(arr) {
+    return arr.map(Function.apply.bind(Math.max, null));
+}
+
+function largestOfFour(arr) {
+    return arr.map(function(group) {
+      return group.reduce(function(prev, current) {
+        return current > prev ? current : prev;
+      });
+    });
+}
+
+function largestOfFour(arr, finalArr = []) {
+    return !arr.length
+      ? finalArr
+      : largestOfFour(arr.slice(1), finalArr.concat(Math.max(...arr[0])))
+}
+  
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
