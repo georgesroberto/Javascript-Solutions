11faa86de207050b4ee1be87c490a9a29be11a7d
georgesroberto <georgesroberto21@gmail.com>
2023-06-05 09:10:03 +0300

    Boolean Checker Function

---
+++ Falsy Bouncers.js
@@ -0,0 +1,11 @@
+function bouncer(arr) {
+  const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) filteredArr.push(arr[i]);
+  }
+  return filteredArr;
+}
+
+function bouncerShort(arr) {
+  return arr.filter(Boolean);
+}
