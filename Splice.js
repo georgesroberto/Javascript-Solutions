0cd835bfc8442dc50c98132139e833643ed62149
georgesroberto <georgesroberto21@gmail.com>
2023-06-07 12:52:14 +0300

    Added examples for better understanding

--- Splice.js
+++ Splice.js
@@ -14,3 +14,15 @@ function frankenSplice(arr1, arr2, n) {
     return localArr;
   }
   console.log(frankenSplice([1, 2, 3], [4, 5], 1))
+
+  //Better Notes
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// Inserts at index 1
+console.log(months);
+// Expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, 'May');
+// Replaces 1 element at index 4
+console.log(months);
+// Expected output: Array ["Jan", "Feb", "March", "April", "May"]
